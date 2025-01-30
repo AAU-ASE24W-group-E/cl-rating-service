b44bb258a639812d996e3cefe204ed7f37766b26
Selina Isak <seisak@edu.aau.at>
2025-01-30 11:24:21 +0100

    Add method to get average rating a user has received

--- src/main/java/at/aau/ase/cl/service/RatingService.java
+++ src/main/java/at/aau/ase/cl/service/RatingService.java
@@ -6,17 +6,19 @@ import at.aau.ase.cl.model.RatingEntity;
 import jakarta.transaction.Transactional;
 import io.quarkus.logging.Log;
 
+import java.util.List;
+import java.util.OptionalDouble;
 import java.util.UUID;
 
 public class RatingService {
 
     @Transactional
-    public Rating createRating(Rating rating){
+    public Rating createRating(Rating rating) {
         boolean ratingWithSameId = RatingEntity.find("id = ?1", rating.getId()).firstResultOptional().isPresent();
 
-        if(ratingWithSameId){
+        if (ratingWithSameId) {
             Log.error("A rating with the same id already exists!");
-        }else{
+        } else {
             RatingEntity ratingEntity = RatingMapper.INSTANCE.map(rating);
             ratingEntity.persistAndFlush();
             return RatingMapper.INSTANCE.map(ratingEntity);
@@ -24,12 +26,37 @@ public class RatingService {
         return null;
     }
 
-    public RatingEntity getRatingById(UUID id){
+    public RatingEntity getRatingById(UUID id) {
         RatingEntity ratingEntity = RatingEntity.findById(id);
-        if (ratingEntity != null){
+        if (ratingEntity != null) {
             return ratingEntity;
         }
         Log.debugf("The rating with id %s is not found!", id);
         return null;
+    }
+
+    public List<RatingEntity> getRatingsForUsername(String username) {
+        List<RatingEntity> ratings = RatingEntity.find("rated_username = ?1", username).list();
+        return ratings;
+    }
+
+    public double getAverageRatingForUsername(String username) {
+        List<RatingEntity> ratings = getRatingsForUsername(username);
+        return getAverageRating(ratings);
+    }
+
+    private double getAverageRating(List<RatingEntity> ratingEntityList){
+        if(ratingEntityList.isEmpty()){
+            return 0;
+        }
+        double sum = 0;
+        int numValidRatings = 0;
+        for(RatingEntity ratingEntity : ratingEntityList){
+            if(ratingEntity != null && ratingEntity.rating != null){
+                sum += ratingEntity.rating;
+                numValidRatings++;
+            }
+        }
+        return sum / numValidRatings;
     }
 }
