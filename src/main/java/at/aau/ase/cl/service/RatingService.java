c3ba5813ce4525472b43a5d441911a9e23340405
Selina Isak <seisak@edu.aau.at>
2025-01-30 22:05:53 +0100

    Replace logs by throwing of custom exceptions + add scope annotation

--- src/main/java/at/aau/ase/cl/service/RatingService.java
+++ src/main/java/at/aau/ase/cl/service/RatingService.java
@@ -1,15 +1,16 @@
 package at.aau.ase.cl.service;
 
+import at.aau.ase.cl.api.interceptors.exceptions.RatingAlreadyExistsException;
+import at.aau.ase.cl.api.interceptors.exceptions.RatingNotFoundException;
 import at.aau.ase.cl.api.model.Rating;
 import at.aau.ase.cl.mapper.RatingMapper;
 import at.aau.ase.cl.model.RatingEntity;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
-import io.quarkus.logging.Log;
-
 import java.util.List;
-import java.util.OptionalDouble;
 import java.util.UUID;
 
+@ApplicationScoped
 public class RatingService {
 
     @Transactional
@@ -17,13 +18,11 @@ public class RatingService {
         boolean ratingWithSameId = RatingEntity.find("id = ?1", rating.getId()).firstResultOptional().isPresent();
 
         if (ratingWithSameId) {
-            Log.error("A rating with the same id already exists!");
-        } else {
-            RatingEntity ratingEntity = RatingMapper.INSTANCE.map(rating);
-            ratingEntity.persistAndFlush();
-            return RatingMapper.INSTANCE.map(ratingEntity);
+            throw new RatingAlreadyExistsException("A rating with the same id already exists!");
         }
-        return null;
+        RatingEntity ratingEntity = RatingMapper.INSTANCE.map(rating);
+        ratingEntity.persistAndFlush();
+        return RatingMapper.INSTANCE.map(ratingEntity);
     }
 
     public RatingEntity getRatingById(UUID id) {
@@ -31,8 +30,7 @@ public class RatingService {
         if (ratingEntity != null) {
             return ratingEntity;
         }
-        Log.debugf("The rating with id %s is not found!", id);
-        return null;
+        throw new RatingNotFoundException(String.format("The rating with id %s is not found!", id));
     }
 
     public List<RatingEntity> getRatingsForUsername(String username) {
@@ -45,17 +43,17 @@ public class RatingService {
         return getAverageRating(ratings);
     }
 
-    public int getNumberOfRatingsForUsername(String username){
+    public int getNumberOfRatingsForUsername(String username) {
         return getRatingsForUsername(username).size();
     }
 
-    private double getAverageRating(List<RatingEntity> ratingEntityList){
-        if(ratingEntityList.isEmpty()){
+    private double getAverageRating(List<RatingEntity> ratingEntityList) {
+        if (ratingEntityList.isEmpty()) {
             return 0;
         }
         double sum = 0;
-        for(RatingEntity ratingEntity : ratingEntityList){
-            if(ratingEntity != null){
+        for (RatingEntity ratingEntity : ratingEntityList) {
+            if (ratingEntity != null) {
                 sum += ratingEntity.rating;
             }
         }
